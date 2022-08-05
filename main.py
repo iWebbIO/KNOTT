1e4f4600ff41d3ff8a0ca0d2fa1ee8cc6bae2042
asksharifi <82875181+asksharifi@users.noreply.github.com>
2022-08-05 22:09:35 +0430

    Added the base code

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 
+import os
+from keep_alive import keep_alive
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="kn ")
+
+
+@bot.event
+async def on_ready():
+    print("Logged in to Knott-BOT")
+    print(bot.user)
+
+@bot.event
+async def on_guild_join(guild):
+	status = discord.game(name=f"On {len(bot.guilds)}", type=3)
+	await bot.change_presence(discord.status.online, status=status)
+	
+
+
+keep_alive()
+bot.run(os.environ.get("BOT_TKN"))
